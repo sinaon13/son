d70dbc7e2fcf7c0b9ca9437eb146e58b5a26ed48
sinaosooli1384@gmail.com <sinaosooli1384@gmail.com>
2024-01-25 21:20:24 +0330

    made 'userSettings' fucntion and changed 'repoEXists' a little

--- son.c
+++ son.c
@@ -3,29 +3,79 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdlib.h>
 
-int repoExists()
+int repoExists(char *address)
 {
-    char cwd[1000];
-    getcwd(cwd, 1000);
+    getcwd(address, 1000);
+    char temp[1000];
+    getcwd(temp, 1000);
     int flag = 0;
     while (1)
     {
-        if (strcmp(cwd, "C:\\") == 0)
-            return 0;
+        if (strcmp(address, "C:\\") == 0)
+        {
+            address = NULL;
+            chdir(temp);
+            return 1;
+        }
         if (flag != 0)
         {
             chdir("..");
         }
-        getcwd(cwd, 1000);
-        DIR *cur = opendir(cwd);
+        getcwd(address, 1000);
+        DIR *cur = opendir(address);
         struct dirent *dirs = readdir(cur);
         while ((dirs = readdir(cur)) != NULL)
         {
             if (strcmp(".son", dirs->d_name) == 0)
-                return 1;
+            {
+                chdir(temp);
+                return 0;
+            }
         }
         flag++;
+    }
+}
+
+int userSettings(int argc, char *argv[])
+{
+    if (strcmp(argv[2], "-global") == 0)
+    {
+        if (strcmp(argv[3], "user.name") == 0)
+        {
+            FILE *name = fopen("C:\\son\\name.txt", "w");
+            fprintf(name, "%s", argv[4]);
+            fclose(name);
+        }
+        if (strcmp(argv[3], "user.email") == 0)
+        {
+            FILE *email = fopen("C:\\son\\email.txt", "w");
+            fprintf(email, "%s", argv[4]);
+            fclose(email);
+        }
+    }
+    else
+    {
+        char *temp = (char *)malloc(1000);
+        repoExists(temp);
+        printf("%s\n", temp);
+        if (strcmp(argv[2], "user.name") == 0)
+        {
+            strcpy(temp + strlen(temp), "\\.son\\name.txt");
+            printf("%s", temp);
+            FILE *name = fopen(temp, "w");
+            fprintf(name, "%s", argv[3]);
+            fclose(name);
+        }
+        if (strcmp(argv[2], "user.email") == 0)
+        {
+            strcpy(temp + strlen(temp), "\\.son\\email.txt");
+            printf("%s", temp);
+            FILE *email = fopen(temp, "w");
+            fprintf(email, "%s", argv[3]);
+            fclose(email);
+        }
     }
 }
 
@@ -58,11 +108,9 @@ int alreadyExists(int argc, char *argv[], char name[])
 
 int makeRepo(int argc, char *argv[])
 {
-    char cwd[1000];
-    getcwd(cwd, 1000);
-    if (repoExists() == 0)
+    char *address = (char *)malloc(1000);
+    if (repoExists(address) == 1)
     {
-        chdir(cwd);
         mkdir(".son");
     }
     return 0;
@@ -80,5 +128,17 @@ int main(int argc, char *argv[])
     {
         makeRepo(argc, argv);
         return 0;
+    }
+
+    char *temp_check = (char *)malloc(1000);
+    if (repoExists(temp_check) == 1)
+    {
+        printf("No repo found!");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "config") == 0)
+    {
+        userSettings(argc, argv);
     }
 }
