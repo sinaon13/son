836ecdc60395e3ee836b83ddfdcaf20b4a700c1e
sinaosooli1384@gmail.com <sinaosooli1384@gmail.com>
2024-01-28 00:51:25 +0330

    alias and its functions(alias-read, alias-write and alias-replace)

--- son.c
+++ son.c
@@ -38,6 +38,87 @@ int repoExists(char *address)
     }
 }
 
+int alias_write(int argc, char *argv[])
+{
+    if (strcmp(argv[2], "-global") == 0)
+    {
+        FILE *alias = fopen("C:\\son\\alias.txt", "a");
+        fprintf(alias, "%s %s\n", argv[3] + 6, argv[4]);
+        fclose(alias);
+    }
+    else
+    {
+        char *wd = (char *)malloc(1000);
+        repoExists(wd);
+        strcpy(wd + strlen(wd), "\\alias.txt");
+        FILE *alias = fopen(wd, "a");
+        fprintf(alias, "%s %s\n", argv[2] + 6, argv[3]);
+        fclose(alias);
+        free(wd);
+    }
+    return 0;
+}
+
+char als_list[100][2][100];
+char global_als_list[100][2][100];
+int total_als_count = 0;
+int total_global_als_count = 0;
+
+int alias_read(int argc, char *argv[])
+{
+    // gloabl
+    FILE *global_alias = fopen("C:\\son\\alias.txt", "r");
+    char *line = (char *)malloc(1000);
+    char temp_1[100], temp_2[100];
+    while (fgets(line, 1000, global_alias) != NULL)
+    {
+        sscanf(line, "%s %s - ", temp_1, temp_2);
+        strcpy(global_als_list[total_global_als_count][0], temp_1);
+        strcpy(global_als_list[total_global_als_count][1], temp_2);
+        total_global_als_count++;
+    }
+    fclose(global_alias);
+    // local
+    char *wd = (char *)malloc(1000);
+    repoExists(wd);
+    strcpy(wd + strlen(wd), "\\alias.txt");
+    FILE *local_alias = fopen(wd, "r");
+    while (fgets(line, 1000, local_alias) != NULL)
+    {
+        sscanf(line, "%s %s", temp_1, temp_2);
+        strcpy(als_list[total_als_count][0], temp_1);
+        strcpy(als_list[total_als_count][1], temp_2);
+        total_als_count++;
+    }
+    fclose(local_alias);
+    free(wd);
+    free(line);
+    return 0;
+}
+
+int alias_replace(int argc, char *argv[])
+{
+    // local
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = total_als_count - 1; j >= 0; j--)
+        {
+            if (strcmp(argv[i], als_list[j][0]) == 0)
+                strcpy(argv[i], als_list[j][1]);
+        }
+    }
+    // global
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = total_global_als_count - 1; j >= 0; j--)
+        {
+            if (strcmp(argv[i], global_als_list[j][0]) == 0)
+                strcpy(argv[i], global_als_list[j][1]);
+        }
+    }
+    return 0;
+}
+
 int userSettings(int argc, char *argv[])
 {
     if (strcmp(argv[2], "-global") == 0)
@@ -91,6 +172,10 @@ int validInput(int argc, char *argv[])
         return 1;
     if (argc == 2 && strcmp(argv[1], "init") == 0)
         return 1;
+    if (argc == 5 && strcmp(argv[1], "config") == 0 && strcmp(argv[2], "-global") == 0 && (strstr(argv[3], "alias.") != NULL))
+        return 1;
+    if (argc == 4 && strcmp(argv[1], "config") == 0 && (strstr(argv[2], "alias.") != NULL))
+        return 1;
     return 0;
 }
 
@@ -118,6 +203,15 @@ int makeRepo(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
+    alias_read(argc, argv);
+
+    alias_replace(argc, argv);
+
+    // for (int i = 0; i < argc; i++)
+    // {
+    //     printf("%s ", argv[i]);
+    // }
+
     if (validInput(argc, argv) == 0)
     {
         printf("Invalid Input!");
@@ -137,8 +231,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (strcmp(argv[1], "config") == 0)
+    if (strcmp(argv[1], "config") == 0 && (strstr(argv[2], "user.") != NULL || strstr(argv[3], "user.") != NULL))
     {
         userSettings(argc, argv);
+    }
+
+    if (strcmp(argv[1], "config") == 0 && (strstr(argv[2], "alias.") != NULL || strstr(argv[3], "alias.") != NULL))
+    {
+        alias_write(argc, argv);
     }
 }
